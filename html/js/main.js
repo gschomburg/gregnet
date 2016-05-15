f812a770c8329893ad823782a3ee6a3a7aafffca
gschomburg <gschomburg@gmail.com>
2016-05-14 22:56:21 -0400

    organizing

---
+++ html/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	console.log("hw");
+
+	load = function(){
+		$.getJSON( "projects.json").done(function( data ) 
+		{
+			var projectTemplate = $('#projectTemplate').html();
+			Mustache.parse(projectTemplate);
+			console.log("done", projectTemplate);
+			$.each( data.project_tiles, function( i, project ) {
+				//$( "<li><img>" ).attr( "src", project.poster ).appendTo( "#tiles" );
+				
+				var rendered = Mustache.render(projectTemplate, {project: project});
+  				$('#tiles').append(rendered);
+				
+
+
+				// if ( i === 2 ) {
+				// 	return false;
+				// }
+			});
+		});
+		  /*
+		  $.each( data, function( key, val ) {
+		    items.push( "<li id='" + key + "'>" + val + "</li>" );
+		  });
+		 
+		  $( "<ul/>", {
+		    "class": "my-new-list",
+		    html: items.join( "" )
+		  }).appendTo( "body" );
+		});
+		*/
+	}
+	load();
+});
